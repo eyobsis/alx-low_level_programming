7e88a4a8d6317178c9de1892e0f7909b99ab2a15
eyobsis <ehtube2020@gmail.com>
2023-02-17 16:52:50 -0800

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main-a program that runs a c fun
+*Return: 0 a value of (success)
+*/
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
