cae6568b6d68bd20aa0d2c5fba5b074e8d172f49
eyobsis <ehtube2020@gmail.com>
2023-02-27 14:12:21 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+char *guard = dest;
 
-	for (; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+while (*src)
+{
+*dest++ = *src++;
+*dest = 0;
+}
+return (guard);
 }
