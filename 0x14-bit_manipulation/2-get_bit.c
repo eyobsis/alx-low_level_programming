a93d404e04501275a473d7d3c3443c3a50ee3e25
eyobsis <ehtube2020@gmail.com>
2023-04-06 12:28:23 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,12 +7,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int mybits;
+	unsigned int i;
 
-	mybits = (n >> index) & 1;
+	i = (n >> index) & 1;
 
-	if ((mybits == 1) || (mybits == 0))
-		return (mybits);
+	if ((i == 1) || (i == 0))
+		return (i);
 	else
 		return (-1);
 }
