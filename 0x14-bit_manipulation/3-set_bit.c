4c626de35a026ae7f6bdf1950a0fd506c4e12e72
eyobsis <ehtube2020@gmail.com>
2023-04-06 12:44:38 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,14 +7,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i;
+unsigned long int i = n ^ m;
+unsigned int count = 0;
 
-	if (n == NULL)
-		return (-1);
+while (i > 0)
+{
+count += i & 1;
+i >>= 1;
+}
 
-	i = 1 << index;
-
-	*n = *n | i;
-
-	return (1);
+return (count);
 }
