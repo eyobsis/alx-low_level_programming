ec1cec5bb09b7f90f7bef76a9c9ec1e67f3ee8e5
eyobsis <ehtube2020@gmail.com>
2023-04-06 13:25:38 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - Entry Point
+ * Return: 0
+ */
+
+int get_endianness(void)
+{
+    unsigned int x = 1;
+    char *c = (char *) &x;
+
+    return (*c);
+}
