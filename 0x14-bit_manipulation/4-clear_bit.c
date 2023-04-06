9730f7cd7250ddffc673705fd01d95a7430e0182
eyobsis <ehtube2020@gmail.com>
2023-04-06 12:36:34 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,15 +7,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int eyobbits;
+	if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
 
-	if (n == NULL)
-		return (-1);
-
-	eyobbits = 1 << index;
-
-	if ((eyobbits | *n) == *n)
-		*n = *n ^ eyobbits;
-
-	return (1);
+    *n &= ~(1UL << index);
+    return (1);
 }
