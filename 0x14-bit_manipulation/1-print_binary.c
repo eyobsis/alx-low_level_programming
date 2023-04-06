62248f5be34170d27d05b3bc436eb374edcadd75
eyobsis <ehtube2020@gmail.com>
2023-04-06 12:15:43 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,6 +2,7 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to print in binary
+ * Return: 0
  */
 void print_binary(unsigned long int n)
 {
@@ -10,14 +11,14 @@ int flag = 0;
 while (mask > 0)
 {
 if ((n & mask) == 0 && flag == 1)
-_putchar('0');
+printf('0');
 else if ((n & mask) != 0)
 {
-_putchar('1');
+printf('1');
 flag = 1;
 }
 mask = mask >> 1;
 }
 if (flag == 0)
-_putchar('0');
+printf('0');
 }
