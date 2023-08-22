1184fe07742c525fd097deea937746850632ca97
eyobsis <ehtube2020@gmail.com>
2023-08-21 17:27:16 -0700

    task-o

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *guard = dest;
+
+while (*src)
+{
+*dest++ = *src++;
+*dest = 0;
+}
+return (guard);
+}
